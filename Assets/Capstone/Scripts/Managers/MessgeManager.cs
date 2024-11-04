93125885645a7e0166ba1d34847b424fb8d30129
ptk57581 <ptk725739@gmail.com>
2024-11-04 10:00:27 +0900

    Message Manager Script

--- Assets/Capstone/Scripts/Managers/MessgeManager.cs
+++ Assets/Capstone/Scripts/Managers/MessgeManager.cs
@@ -7,12 +7,32 @@ public class MessgeManager : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject board;
+    private GameObject display;
     [SerializeField]
     private TMP_Text aletText;
 
+    public float displayDuration;
+
     public void Notification(string message)
     {
+        display.SetActive(true);
         aletText.text = message;
+        StartCoroutine(DeactivateAfterDelay(displayDuration));
+    }
+    private IEnumerator DeactivateAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        DeactivateDisplay();
+    }
+    private void DeactivateDisplay()
+    {
+        aletText.text = "";
+        display.SetActive(false);        
+    }
+
+    [ContextMenu("Test")]
+    public void TEST_notification()
+    {
+        Notification("notice Text!");
     }
 }
